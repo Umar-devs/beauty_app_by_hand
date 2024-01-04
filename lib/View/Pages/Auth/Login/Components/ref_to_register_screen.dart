359b30039ad4b3b3f1776dcad917499f7dd06fa8
Aethon-developer <aethon.developer@gmail.com>
2024-01-04 14:02:07 +0500

    update

--- lib/View/Pages/Auth/Login/Components/ref_to_register_screen.dart
+++ lib/View/Pages/Auth/Login/Components/ref_to_register_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../../../Widgets/Reusable Components/reusable_txt.dart';
 import '../../Register/register_screen.dart';
 
